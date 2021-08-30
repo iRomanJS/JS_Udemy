051257fe462b897960acb6b9caa48d053d604099
iRomanJS <leshchukroman@gmail.com>
2021-08-31 00:42:28 +0300

    dicided to retype this tasks

--- lessons/015/015_script.js
+++ lessons/015/015_script.js
@@ -12,42 +12,40 @@
 
 4) Потренироваться и переписать цикл еще двумя способами*/
 
-'use strict';
-
+"use strict";
 
 const numberOfFilms = prompt("Сколько фильмов вы уже посмотрели?", "");
 
-let personalMovieDB = {
+const personalMovieDB = {
     count: numberOfFilms,
-    movies:{},
-    actors:{},
-    generes:[],
+    movies: {},
+    actors: {},
+    genres: [],
     privat: false
-};
+}
 
 for (let i = 0; i < 2; i++) {
     const a = prompt("Один из последних просмотренных фильмов?", ""),
-          b = prompt("На сколько оцените его?", "");
-    
-    if (a != null && b != null && a !='' && b != '' && a.length < 50) {
+        b = prompt("На сколько оцените его?", "");
+    if (a != null && b != null && a != '' && b != '' && a != a.leght < 50) {
         personalMovieDB.movies[a] = b;
-        console.log('Да');
+        console.log("Спасибо ответы сохранил");
     } else {
-        console.log('Error');
+        console.log("Заполняйте поля коректно");
         i--;
     }
-    
-     
+          
 }
 
 if (personalMovieDB.count < 10) {
-    console.log("Просмотрено довольно мало фильмов");
+    alert("Просмотрено довольно мало фильмов");
 } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
-    console.log("Вы классический зритель");
-} else if (personalMovieDB.count >=30) {
-    console.log("Вы киноман");
+    alert("Вы классический зритель");
+} else if (personalMovieDB.count >= 30) {
+    alert("Вы киноман");
 } else {
-    console.log("Произошла ошибка");
-}
+    alert("Произошла ошибка");
+
+} 
 
 console.log(personalMovieDB);
